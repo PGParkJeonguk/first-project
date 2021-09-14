580e971ccb3538147e47464d986728d87cb41911
admin <admin@YD02-22>
2021-09-14 17:49:00 +0900

    변수 설명

--- firstProject/src/firstProject/ParkJeongUk.java
+++ firstProject/src/firstProject/ParkJeongUk.java
@@ -8,6 +8,8 @@ public class ParkJeongUk {
 		System.out.println(num1+num2);
 		
 		Scanner scn = new Scanner(System.in);
+		System.out.println("이름을 입력하세요.");
+		String name = scn.nextLine();
 		System.out.println("영어성적을 입력하세요.");
 		int english = scn.nextInt();
 		scn.nextLine();
@@ -16,10 +18,15 @@ public class ParkJeongUk {
 		scn.nextLine();
 		System.out.println("수학성적을 입력하세요.");
 		int math = scn.nextInt();
-		System.out.println("이름을 입력하세요.");
-		String name = scn.nextLine();
-		scn.nextLine();
 		
-		System.out.printf("%s님의, 총합성적은 %d, 평균은 %d 입니다.",name , english+history+math, (english+history+math)/3);
+		System.out.printf("%s님의, 총합성적은 %d, 평균은 %d 입니다.\n",name , english+history+math, (english+history+math)/3);
+		
+		Boolean pass = math+english+history >=180;
+		
+		if(pass) {
+			System.out.println(name+"님은 합격하였습니다.");
+		}else {
+			System.out.println(name+"님은 불합격하였습니다.");
+		}
 	}
 }
